3051aa769af2586410a9d14f7fd9a579cd5e75ce
tiurmagrace <tiursihaloho210905@gmail.com>
2025-03-04 11:05:04 +0700

    Menambahkan implementasi state-based construction untuk DoorMachine

---
+++ nomor4_TugasPendahuluan/DoorMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class DoorMachine
+{
+    private enum State { Terkunci, Terbuka }
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = State.Terkunci; // State awal
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void BukaPintu()
+    {
+        if (currentState == State.Terkunci)
+        {
+            currentState = State.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+        else
+        {
+            Console.WriteLine("Pintu sudah terbuka");
+        }
+    }
+
+    public void KunciPintu()
+    {
+        if (currentState == State.Terbuka)
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+        else
+        {
+            Console.WriteLine("Pintu sudah terkunci");
+        }
+    }
+}
+
